7036eb117a97f23a746c69b15e779d86e57636a2
Andrew Leggett <aleggett@scca.connectionsacademy.org>
2019-12-04 14:19:36 -0500

    developed a working implementation

--- addRecipientSearch.js
+++ addRecipientSearch.js
@@ -9,9 +9,8 @@ const ccrCWS = { //namespace things to prevent accidental overwrites
         }
     },
     addSearch() { //add the search box and a clarifying label to the top of the page
-        let head = document.getElementById("selectRecipientsHeader"); //get the element we want to insert the search box inside of
-        //add HTML for our searchbox and a descriptive label into the element
-        head.parentElement.innerHTML+= `<br><label class="primaryLabel" for="listName">Filter Available Recipients: </label>
+        //add HTML for our searchbox and a descriptive label into a suitable part of the page
+        document.getElementsByClassName("formRow addressBookList ng-scope")[0].insertAdjacentHTML("afterend", `<br><label class="primaryLabel" for="listName">Filter Available Recipients: </label>
             <br><table class="ccr_t">
                 <tr class="ccr_tr">
                     <td class="ccr_td">
@@ -21,29 +20,48 @@ const ccrCWS = { //namespace things to prevent accidental overwrites
                         <div id="ccrSpinner" hidden="true" class="ccr_loader"></div>
                     </td>
                 </tr>
-            </table>`;
+            </table>`);
         ccrSearchbox.onkeyup = function() { //declare a function to run whenever someone releases a key inside the search box
-            if (document.getElementById("recipients").options.length > 500) {
+            if (document.getElementById("recipients").options.length > 200) { //if the recipients list is long - and likely to lead to an unpleasant user experience with a more simple option hiding approach,
                 ccrSpinner.hidden = false; //reveal a spinner to reassure users that the browser has not totally crashed
-                ccrCWS.timeout = null; //if this has already been triggered, abort triggering it
                 ccrCWS.searchText = ccrSearchbox.value; //record what was typed so we can not run the search if the user was still typing
-                ccrCWS.timeout = setTimeout(ccrCWS.search, 1000); //trigger the search after 1 second
-            } else {
+                setTimeout(ccrCWS.bigSearch, 300); //run the alternative search function for handling large lists - after a pause so that we run fewer searches during fast typing
+            } else { //otherwise,
                 ccrSpinner.hidden = false; //reveal a spinner to reassure users that the browser has not totally crashed
                 ccrCWS.searchText = ccrSearchbox.value; //record what was typed so search works
-                ccrCWS.search(); //run the search
+                setTimeout(ccrCWS.search, 300); //run the search after a pause, so that we run fewer searches during fast typing
             }
         }
     },
-    searchText: "",
-    timeout: null,
+    searchText: "", //property to hold our record of what was typed into the search box
     async search() { //search for users matching what was typed
         let text = ccrSearchbox.value; //get the text they've entered
-        if (ccrCWS.searchText !== text) {
+        if (ccrCWS.searchText !== text) { //see whether the text entered when the function was called matches what's in the search box right now - this reduces the number of unneccessary function calls and delays searching while a user is typing quickly
+            ccrSpinner.hidden = true; //hide the spinner, and finish there until there's a 300ms pause in typing
+        } else {
+            text = text.toLowerCase(); //convert that text to lower case - so we can match things without regard for case
+            let options = document.getElementById("recipients").options; //get the list of recipients in the available recipients box
+            for (let i=0;i<options.length;i++) { //iterate over these recipients
+                let o = ""; //declare a string
+                o += options[i].innerText; //add the recipient's name to that string
+                o = o.toLowerCase(); //convert the recipient's name to lower case so we can match without regard for case
+                if (o.indexOf(text) === -1) { //if the text entered in the search box does not appear in the recipient name text,
+                    options[i].hidden = true; //hide the recipient
+                } else { //otherwise, if it does appear,
+                    options[i].hidden = false; //unhide the recipient
+                }
+            }
+            ccrSpinner.hidden = true; //hide the spinner
+        }
+    },
+    async bigSearch() { //same as above, but this time we hide the whole recipients box while we make changes to dodge issues associated with making thousands of DOM changes in quick succession (serious crashing issues were seen when testing)
+        let text = ccrSearchbox.value; //get the text they've entered
+        if (ccrCWS.searchText !== text) { //see whether the text entered when the function was called matches what's in the search box right now - this reduces the number of unneccessary function calls and delays searching while a user is typing quickly
             ccrSpinner.hidden = true; //hide the spinner
         } else {
             text = text.toLowerCase(); //convert that text to lower case - so we can match things without regard of case
             let options = document.getElementById("recipients").options; //get the list of recipients in the available recipients box
+            document.getElementById("recipients").hidden = true; //hide the recipients list to reduce strain
             for (let i=0;i<options.length;i++) { //iterate over these recipients
                 let o = ""; //declare a string
                 o += options[i].innerText; //add the recipient's name to that string
@@ -54,12 +72,13 @@ const ccrCWS = { //namespace things to prevent accidental overwrites
                     options[i].hidden = false; //unhide the recipient
                 }
             }
+            document.getElementById("recipients").hidden = false; //reveal the updated recipients box
             ccrSpinner.hidden = true; //hide the spinner
         }
     }
 };
 
-//Add CSS for a spinner
+//Add CSS for a spinner - the loading display the page has already is excessively obtrusive for our purposes
 var styles = `
     .ccr_t {
         border: 0px;
@@ -83,9 +102,9 @@ var styles = `
     }
 `;
 
-var styleSheet = document.createElement("style");
-styleSheet.type = "text/css";
-styleSheet.innerText = styles;
-document.body.appendChild(styleSheet);
+var styleSheet = document.createElement("style"); //begin creating a stylesheet element
+styleSheet.type = "text/css"; //give styleSheet it's text/css type
+styleSheet.innerText = styles; //add the spinner CSS text to the element
+document.body.appendChild(styleSheet); //add the element to the page
 
 ccrCWS.periodically(); //start the program
